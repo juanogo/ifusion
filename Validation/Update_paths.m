bd28947a3de6ad7113413ab86d160bbeb5a21b78
juanogo <38761819+juanogo@users.noreply.github.com>
2018-05-22 09:41:03 -0700

    Add files via upload

---
+++ Validation/Update_paths.m
@@ -0,0 +1,30 @@
+function Update_paths(name)
+% Update_paths(name)
+%
+% SYNOPSIS: Change the path of store cases (actually change variable of a
+%           given .mat file).
+%
+% INPUT:    name: a string containing the patient file name without path neither extention
+%           
+%           path1 is the path of the BPL angio
+%           path2 is the path of the BPR angio
+%           path3 is the path of the APR angio
+%           path4 is the path of the APL angio
+%           path5 is the path of the Coronography angio
+%           path6 is the path of the Pullback IVUS
+%
+% OUTPUT:   None. It modifies the MATLAB archive 'name'.
+%
+% REF:
+%
+% COMMENTS:
+%
+
+load (name)
+
+for i=1:6
+    path = input(['Enter the new path for path' num2str(i) ' (path+filename): '], 's');
+    eval(['path' num2str(i) '=path']);
+end
+
+save(name)
